d590e0c50e2082ffbd2f274f7f7f27dbfe5ff247
Anshcoder098 <ansh88179@gmail.com>
2024-10-03 12:02:08 +0530

    Add files via upload

---
+++ addgivennn.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+	int number;
+	printf("enter a number");
+	scanf("%d",&number);
+	if(number>0){
+		printf("the number is positive");
+	}
+		else if(number<0)
+		{
+			printf("the number is negative \n");
+		}
+		else{
+			printf("the number is zero");
+		}
+		
+		return 0;
+	}
